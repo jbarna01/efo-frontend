e96d0010827239551ba0c0ed031b8dbe6dee8fef
Barna József <jbarna@vipmail.hu>
2022-06-28 23:12:46 +0200

    Excel kulcs generalas indítása

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from "@angular/core";
 import {Router} from "@angular/router";
+import {EfoExcelControllerService} from "../../../build/openapi/efo";
+import {MegerrositesDialogComponent} from "../dialogs/megerrosites-dialog/megerrosites-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-dashboard',
@@ -11,12 +14,30 @@ export class DashboardComponent implements OnInit {
   message: string = '';
 
 
-  constructor(private router: Router) { }
+  constructor(private dialog: MatDialog,
+              private router: Router,
+              private efoExcelControllerService: EfoExcelControllerService) {
+  }
 
   maiNap: Date = new Date();
 
   ngOnInit(): void {
   }
+
+  public excelKlikk() {
+    const title = 'Kulcs Excel generálás';
+    const msg = 'Indítsam az Excel tábla generálását?';
+
+    const dialogRef = this.dialog.open(MegerrositesDialogComponent, {
+      data: {title: title, msg: msg}, disableClose: true
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.efoExcelControllerService.excelExport().subscribe();
+      }
+    });
+  }
+
 
 }
 
